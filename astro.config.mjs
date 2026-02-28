b79c7f2f2637d8c4bf44e69670d9553c235a40a5
Brad Sukala <bradsukala@Negroni.local>
2026-02-27 20:31:15 -0500

    Add privacy policy page and update footer link update canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.fabriklabs.co',
+  site: 'https://fabriklabs.co',
   integrations: [tailwind(), sitemap()],
 });
